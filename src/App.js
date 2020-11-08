88163be70e18f01a3467a782ad66c4702eb65a76
ashandileo <ashandileonadi2001@gmail.com>
2020-11-08 07:00:19 +0700

    Setting up project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    isModalOpen: false
+  }
+
+  showModal = () => {
+    this.setState({ isModalOpen: true })
+  }
+
+  closeModal = () => {
+    this.setState({ isModalOpen: false })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal
+          closed={this.closeModal}
+          show={this.state.isModalOpen}
+        />
+        <Backdrop
+          show={this.state.isModalOpen}
+        />
+        <button
+          className="Button"
+          onClick={this.showModal}
+        >
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
