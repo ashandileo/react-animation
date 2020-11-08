4401e32fd113a0717b5752fee2003e1f396d1a5e
ashandileo <ashandileonadi2001@gmail.com>
2020-11-08 12:19:54 +0700

    Change indent with spaces:2

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,36 +3,36 @@ import { Transition } from 'react-transition-group';
 import './Modal.css';
 
 const animationTiming = {
-    enter: 400,
-    exit: 1000
+  enter: 400,
+  exit: 1000
 }
 
 const modal = (props) => {
-    const modalProps = {
-        in: props.show,
-        timeout: {...animationTiming},
-        mountOnEnter: true,
-        unmountOnExit: true
-    }
-    return (
-        <Transition {...modalProps}>
-            {state => {
-                const cssClasses = [
-                    'Modal',
-                    state === 'entering'
-                    ? 'ModalOpen'
-                    : state === 'exiting'
-                    && 'ModalClosed'
-                ]
-                return (
-                    <div className={cssClasses.join(' ')}>
-                        <h1>A Modal</h1>
-                        <button className="Button" onClick={props.closed}>Dismiss</button>
-                    </div>
-                )}
-            }
-        </Transition>
-    )
+  const modalProps = {
+    in: props.show,
+    timeout: {...animationTiming},
+    mountOnEnter: true,
+    unmountOnExit: true
+  }
+  return (
+    <Transition {...modalProps}>
+      {state => {
+        const cssClasses = [
+          'Modal',
+          state === 'entering'
+          ? 'ModalOpen'
+          : state === 'exiting'
+          && 'ModalClosed'
+        ]
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>Dismiss</button>
+          </div>
+        )}
+      }
+    </Transition>
+  )
 }
 
 export default modal;
