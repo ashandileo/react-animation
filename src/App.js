d966b3bdb0f01cd3fa2356e5385086696f7d7f57
ashandileo <ashandileonadi2001@gmail.com>
2020-11-08 13:30:54 +0700

    Transition events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Transition } from 'react-transition-group';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -25,18 +25,29 @@ class App extends Component {
       <div className="App">
         <h1>React Animations</h1>
         <button className="Button" onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
-        <Transition in={this.state.showBlock} timeout={300} mountOnEnter unmountOnExit>
-          {state => (
-            <div style={{
-                backgroundColor: 'red',
-                width: 100,
-                height: 100,
-                margin: 'auto',
-                transition: 'opacity 300ms ease-out',
-                opacity: state === 'exiting' ? 0 : 1
-              }}
-            />
-          )}
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+          >
+            {state => (
+              <div style={{
+                  backgroundColor: 'red',
+                  width: 100,
+                  height: 100,
+                  margin: 'auto',
+                  transition: 'opacity 300ms ease-out',
+                  opacity: state === 'exiting' ? 0 : 1
+                }}
+              />
+            )}
         </Transition>
         <br />
         <Modal closed={this.closeModal} show={this.state.isModalOpen} />
